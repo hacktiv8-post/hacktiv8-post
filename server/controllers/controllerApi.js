b840c8d74387c00f33cae1515c324c8bc260f1d5
alkindi <kinyobi@pop-os.localdomain>
2021-02-04 19:33:45 +0700

    .

---
+++ server/controllers/controllerApi.js
@@ -0,0 +1,9 @@
+const axios = require('axios')
+
+class ControllerApi {
+    static getNews (req, res, next) {
+      
+    }
+}
+
+module.exports = ControllerApi
--- server/routes/index-route.js
+++ server/routes/index-route.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Controller = require('../controllers/controller')
+const ControllerApi = require('../controllers/controllerApi')
 
 router.post('/register', Controller.register)
 
