87d9ee18ceb0dc21e9a1bae2bebc6953e667d8d5
LearnProcessing <rinaldiadrian5@gmail.com>
2021-02-04 19:55:48 +0700

    error handler added

---
+++ server/middleware/errorhandler.js
@@ -0,0 +1,32 @@
+const errorHandler = (error, req, res, next) => {
+    
+    let errorMessages = []
+    let errorMessage = ''
+    let errorCode = 0
+    switch (error.name) {
+        case 'SequelizeValidationError':
+            errorMessages = error.errors.map(e => {
+                return e.message
+            })
+            res.status(400).json({
+                messages: errorMessages
+            })
+            break
+        case 'SequelizeUniqueConstraintError':
+            errorMessages = error.errors.map(e => {
+                return e.message
+            })
+            res.status(400).json({
+                messages: errorMessages
+            })
+        case 'Custom error':
+            errorMessage = error.message
+            errorCode = error.code
+            res.status(errorCode).json({
+                message: errorMessage
+            })
+    }
+
+}
+
+module.exports = { errorHandler }
--- server/routes/index-route.js
+++ server/routes/index-route.js
@@ -3,5 +3,6 @@ const Controller = require('../controllers/controller')
 
 router.post('/register', Controller.register)
 router.post('/login', Controller.login)
+// router.get('/dashboard', Controller.dashboard)
 
 module.exports = router
